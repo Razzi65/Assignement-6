e919f6c05ed88e9eb0bac8bfb8dc8733c3206768
Razzi65 <rizmoh65@gmail.com>
2022-09-06 02:25:23 +0500

    second ver

--- index.ts
+++ index.ts
@@ -1,8 +1,17 @@
 //1. Write a ts program to print all natural numbers from 1 to n. - using while loop
 
-var n:number=20;
-var i:number=1;
-while (i<n) {
-    console.log(i);
-    i++;
-}
+// var n:number=20;
+// var i:number=1;
+// while (i<n) {
+//     console.log(i);
+//     i++;
+// }
+
+//2. Write a ts program to print all natural numbers in reverse (from n to 1). - using while loop
+
+// var n:number=20;
+// var i:number=0;
+// while (i<n){
+//     console.log(n-i);
+//     i++
+// }
