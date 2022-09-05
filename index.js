b942033df27879a1f42a08728e3dd044c8ff90ef
Razzi65 <rizmoh65@gmail.com>
2022-09-06 02:13:32 +0500

    First version

---
+++ index.js
@@ -0,0 +1,7 @@
+//1. Write a ts program to print all natural numbers from 1 to n. - using while loop
+var n = 20;
+var i = 1;
+while (i < n) {
+    console.log(i);
+    i++;
+}
---
+++ index.ts
@@ -0,0 +1,8 @@
+//1. Write a ts program to print all natural numbers from 1 to n. - using while loop
+
+var n:number=20;
+var i:number=1;
+while (i<n) {
+    console.log(i);
+    i++;
+}
